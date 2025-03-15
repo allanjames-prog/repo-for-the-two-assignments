fe77f26828771db67ff6bac73f79adfd13845d5e
pervincathie <pervincathie724@gmail.com>
2025-03-15 16:19:23 +0300

    Assignments

---
+++ nanyonga pervin/module1.py
@@ -0,0 +1,9 @@
+import inheritance 
+
+mike = inheritance.Net_pay("Mrs. Orban", "Manager")
+mike.departments("Management")
+mike.role()
+mike.salary(570000)
+mike.imposed(0.02)
+print(mike.calc())
+print(mike.total())
---
+++ nanyonga pervin/module2.py
@@ -0,0 +1,5 @@
+import group5
+
+
+#calling the function to excute
+group5.capture_students()
